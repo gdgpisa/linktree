628d39452d22876fc3492ed01d923a8262035870
Davide <70480082+DavideBri@users.noreply.github.com>
2025-03-26 18:43:00 +0000

    Last and Next event are now separeted

--- src/components/LatestEventLink.tsx
+++ src/components/LatestEventLink.tsx
@@ -10,17 +10,32 @@ type GDGComunityEvent = {
 
 async function getLatestEvent(): Promise<GDGComunityEvent> {
     const res = await fetch(
-        `https://gdg.community.dev/api/event_slim/for_chapter/854/?page_size=4&status=Completed&include_cohosted_events=true&visible_on_parent_chapter_only=true&order=-start_date&fields=title,start_date,event_type_title,cropped_picture_url,cropped_banner_url,url,cohost_registration_url,description,description_short&page=1`,
+        `https://gdg.community.dev/api/event_slim/for_chapter/854/?page_size=1&include_cohosted_events=true&visible_on_parent_chapter_only=true&order=-start_date&fields=title,start_date,event_type_title,cropped_picture_url,cropped_banner_url,url,cohost_registration_url,description,description_short&page=1`,
         {
             mode: 'cors',
         },
     )
 
     const data = await res.json()
-
     const { results } = data
 
-    console.log(results[0])
+    console.log('Latest Upcoming Event:', results[0])
+
+    return results[0]
+}
+
+async function getLatestCompletedEvent(): Promise<GDGComunityEvent> {
+    const res = await fetch(
+        `https://gdg.community.dev/api/event_slim/for_chapter/854/?page_size=1&status=Completed&include_cohosted_events=true&visible_on_parent_chapter_only=true&order=-start_date&fields=title,start_date,event_type_title,cropped_picture_url,cropped_banner_url,url,cohost_registration_url,description,description_short&page=1`,
+        {
+            mode: 'cors',
+        },
+    )
+
+    const data = await res.json()
+    const { results } = data
+
+    console.log('Latest Completed Event:', results[0])
 
     return results[0]
 }
@@ -29,13 +44,18 @@ const FEEDBACK_FORM_URL_PREFIX = `https://docs.google.com/forms/d/e/1FAIpQLSdPCr
 
 export const LatestEventLink = ({}) => {
     const [latestEvent, setLatestEvent] = useState<GDGComunityEvent | null>(null)
+    const [latestCompletedEvent, setLatestCompletedEvent] = useState<GDGComunityEvent | null>(null)
 
     useEffect(() => {
         getLatestEvent().then(event => {
             setLatestEvent(event)
         })
+        getLatestCompletedEvent().then(event => {
+            setLatestCompletedEvent(event)
+        })
     }, [])
 
+    
     return (
         <>
             {latestEvent && (
@@ -48,7 +68,9 @@ export const LatestEventLink = ({}) => {
                     <a
                         href={
                             FEEDBACK_FORM_URL_PREFIX +
-                            `?usp=pp_url&entry.1235423955=${encodeURIComponent(latestEvent.title)}`
+                            `?usp=pp_url&entry.1235423955=${
+                                latestCompletedEvent ? encodeURIComponent(latestCompletedEvent.title) : ''
+                            }`
                         }
                         class="large"
                     >
